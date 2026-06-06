67cadd35ea2650f1d37c09951c76adfdc00a06e6
Lidia Marpaung <lidia.123450015@student.itera.ac.id>
2026-06-07 02:51:34 +0700

    Delete lain.py

--- lain.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.getOrCreate()
-
-df = spark.read.parquet("dataset_terpadu.parquet")
-
-df.select("bulan").distinct().show(20, False)
